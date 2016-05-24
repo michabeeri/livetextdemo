f302345dac8232d7e0337af4a43ab93a2200018f
michabeeri <michaelb@wix.com>
2016-05-24 20:26:50 +0300

    including third party this time

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ requirejs.config({
     baseUrl: 'src',
     paths: {
         lodash: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.3.0/lodash',
-        libphonenumber: "../node_modules/google-libphonenumber/dist/browser/libphonenumber.min"
+        libphonenumber: "thirdparty/libphonenumber.min"
     },
     shim: {
         lodash: { exports: '_' }
