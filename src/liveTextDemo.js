cb46099733b8126a2e208d72b3d19210166a912e
michabeeri <michaelb@wix.com>
2016-08-31 14:30:25 +0300

    font tool

--- src/liveTextDemo.js
+++ src/liveTextDemo.js
@@ -1,59 +1,66 @@
 'use strict';
 
-define(['lodash', 'textPatternRecognizer'], function (_, textPatternRecognizer) {
+define(['lodash'], function (_) {
 
-    function createPhoneAnchorTag (item) {
-        return "<a class=\"phoneLink\" href=\"tel:" + item.value + "\">" + item.key + "</a>";
+    var previousShowAlways = false;
+    var permanentShowAlways = false;
+    var temporaryShowAlways = false;
+
+    var addressInput = document.getElementById('address-input');
+    addressInput.addEventListener('keyup', function (e){
+        if(e.key === 'Enter') {
+            handleAddress();
+        }
+    });
+
+    var debouncedHide = _.debounce(
+        function() {
+            temporaryShowAlways = false;
+            setShowAlways();
+        },
+        300,
+        {'leading': false, 'trailing': true});
+    window.addEventListener('keydown', function (e){
+        if(e.key === 'S') {
+            e.preventDefault();
+            temporaryShowAlways = true;
+            debouncedHide();
+            setShowAlways();
+        }
+    });
+
+    var openButton = document.getElementById('open-button');
+    openButton.addEventListener('click', handleAddress);
+
+    var showAlwaysCheckBox = document.getElementById('show-always-input');
+    showAlwaysCheckBox.addEventListener('change', onShowAlwaysCheckboxChange);
+
+    var desktopSite = document.getElementById('desktop-site');
+    var regularSite = document.getElementById('regular-site');
+    var newSite = document.getElementById('new-site');
+    handleAddress();
+
+
+    function handleAddress(){
+        var address = addressInput.value;
+        desktopSite.src = address + '?ReactSource=d793eaebc1f3ca098d092d5ce44db9593ab5ad3a';
+        regularSite.src = address + '?ReactSource=d793eaebc1f3ca098d092d5ce44db9593ab5ad3a&showMobileView=true';
+        newSite.src = address + '?ReactSource=d793eaebc1f3ca098d092d5ce44db9593ab5ad3a&showMobileView=true&experiments=newFontTransform';
     }
 
-    function createMailAnchorTag (item) {
-        return "<a class=\"mailLink\" href=\"mailto:" + item.value + "\">" + item.key + "</a>";
-    }
-
-    function createUrlAnchorTag (item) {
-        return "<a class=\"url\" href=\"" + item.value + "\">" + item.key + "</a>";
-    }
-
-    function MarkPhones (htmlContent) {
-        var recognizedItems = textPatternRecognizer.findPatterns(htmlContent, countryCode.value)
-
-        var processedItems = _(recognizedItems)
-            .orderBy('index', 'desc')
-            .map(function(item) {
-                switch (item.pattern) {
-                   case textPatternRecognizer.Pattern.PHONE :
-                       return _.assign({}, item, {markup: createPhoneAnchorTag(item)});
-
-                   case textPatternRecognizer.Pattern.MAIL :
-                       return _.assign({}, item, {markup: createMailAnchorTag(item)});
-
-                   case textPatternRecognizer.Pattern.URL :
-                       return _.assign({}, item, {markup: createUrlAnchorTag(item)});
-
-                   throw "Unknown patternType";
-                }
+    function setShowAlways() {
+        console.log(permanentShowAlways || temporaryShowAlways);
+        var show = permanentShowAlways || temporaryShowAlways;
+        if (previousShowAlways !== show) {
+            previousShowAlways = show;
+            _.forEach([desktopSite, regularSite, newSite], function(iframe){
+                iframe.contentWindow.postMessage('show-always', '*');
             })
-            .value();
-
-        _.forEach(processedItems, function (item) {
-            htmlContent = replaceWithAnchorTag(htmlContent, item);
-        });
-
-        return htmlContent;
+        }
     }
 
-    function replaceWithAnchorTag(htmlContent, item) {
-        return htmlContent.slice(0, item.index) + item.markup + htmlContent.slice(item.index + item.key.length);
+    function onShowAlwaysCheckboxChange(event) {
+        permanentShowAlways = event.currentTarget.checked;
+        setShowAlways();
     }
-
-    function generateText () {
-        outputContainer.innerHTML = MarkPhones(inputContainer.value);
-    }
-    
-    var outputContainer = document.getElementById("output-container");
-    var inputContainer = document.getElementById("input-container");
-    var countryCode = document.getElementById("country-code");
-    inputContainer.addEventListener("keyup", generateText);
-    countryCode.addEventListener("keyup", generateText);
-
 });
